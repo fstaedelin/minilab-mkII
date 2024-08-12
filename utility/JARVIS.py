fcd6dfba3bf96f9d2664fd8fcc0ffa0f2ca55d34
Félix STAEDELIN-HOYO <felix.staedelin-hoyo@univ-amu.fr>
2024-08-12 20:40:49 +0200

    Some refactoring

--- utility/JARVIS.py
+++ utility/JARVIS.py
@@ -3,3 +3,17 @@ from midi_check.MIDI_CHECK import MIDI_CHECK as MC
 #-----------------------------------------------------------------------------------------
 # Set up the logger with the coolest name
 _JARVIS = MC("DEBUG")
+
+# Add a test to check if a given value is greater than 100.
+# This test returns True if the value exceeds 100, otherwise False.
+is_more_100 = _JARVIS.AddTest(
+    test_fn=lambda x: x > 100,
+    name=">100"
+)
+
+# Add a test to check if a given value is greater than 150.
+# This test returns True if the value exceeds 150, otherwise False.
+is_more_150 = _JARVIS.AddTest(
+    test_fn=lambda x: x > 150,
+    name=">150"
+)
